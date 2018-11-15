44694ec4caa7478b7d51193c522ea86b621bd832
Archaic Archon <archaicarchon@gmail.com>
2018-11-15 00:26:07 +0000

    fixed an error and REALLY REMOVED exectuables files this time!

--- cloudy-datatypes/src/Cloudy/Mud/Misc.hs
+++
@@ -1,7 +0,0 @@
-module Cloudy.Mud.Misc where
-
-debug :: Bool
-debug = True
-
-defaultTimeout :: Int
-defaultTimeout = 1000*1000
--- install.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-cabal update
-cd cloudy-datatypes
-cabal install
-cd ..
-cd cloudy-front
-cabal install
-cd ..
-echo Cloudy Front Installed!
